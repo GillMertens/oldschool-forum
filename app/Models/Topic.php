1234e12f3ab347460e4a19163adbd5ea51356115
Gill Mertens <gill.mertens@student.ehb.be>
2024-01-03 16:49:15 +0100

    Add models

---
+++ app/Models/Topic.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tag()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
