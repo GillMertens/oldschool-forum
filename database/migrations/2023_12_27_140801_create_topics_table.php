e12311a9d07b29fdd135748b1f8de02d7a9a890b
Gill Mertens <gill.mertens@student.ehb.be>
2024-01-03 19:23:44 +0100

    Fixed registration process to include all fields

--- database/migrations/2023_12_27_140801_create_topics_table.php
+++ database/migrations/2023_12_27_140801_create_topics_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('topics', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('color');
             $table->string('image_url');
             $table->timestamps();
 
