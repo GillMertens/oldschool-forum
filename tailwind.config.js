5e2995b03dae2f4d58950d074b48c6b1aa3d9920
Gill Mertens <gill.mertens@student.ehb.be>
2024-01-04 15:26:20 +0100

    Add custom tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+const defaultTheme = require('tailwindcss/defaultTheme');
+const forms = require('@tailwindcss/forms');
 const plugin = require('tailwindcss/plugin');
 
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -15,6 +14,26 @@ export default {
             fontFamily: {
                 sans: ['Inter', ...defaultTheme.fontFamily.sans],
             },
+            fontSize: {
+                'down-6': '0.4355em',
+                'down-5': '0.5em',
+                'down-4': '0.5745em',
+                'down-3': '0.6599em',
+                'down-2': '0.7579em',
+                'down-1': '0.8706em',
+                'base': '1em',
+                'up-1': '1.1487em',
+                'up-2': '1.3195em',
+                'up-3': '1.5157em',
+                'up-4': '1.7511em',
+                'up-5': '2em',
+                'up-6': '2.296em',
+            },
+            colors: {
+                'primary': '#000000',
+                'primary-high': '#4d4d4d',
+                'primary-low': '#e6e6e6',
+            }
         },
     },
 
