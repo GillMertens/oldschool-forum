ca73c29dbf3d5a584f4430aef03ad13cc1b1e221
Gill Mertens <gill@clickcare.be>
2024-08-10 17:02:42 +0200

    Fix: bug in profile update request validation

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,10 +16,10 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name' => ['required', 'string', 'max:255'],
-            'last_name' => ['nullable', 'string', 'max:255'],
-            'username' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'first_name' => ['string', 'max:255'],
+            'last_name' => ['string', 'max:255'],
+            'username' => ['string', 'max:255', 'unique:users,username,' . $this->user()->id],
+            'email' => ['string', 'lowercase', 'email', 'max:255', 'unique:users,email,' . $this->user()->id],
         ];
     }
 }
