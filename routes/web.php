1aadd7a6cd1eb3fe5d1ed45b000f481df4b968fe
Gill Mertens <gill.mertens@student.ehb.be>
2024-08-12 00:10:28 +0200

    Chore: Routes cleanup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FaqAnswerController;
 use App\Http\Controllers\FaqController;
@@ -21,29 +22,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[DashboardController::class, 'index'])->name('dashboard');
-
-
-Route::post('/topics', [TopicController::class, 'store'])->name('topics.store');
-
 Route::get('/topics/{topic:slug}', [TopicController::class, 'show'])->name('topics.show');
-
-
-Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-
-Route::post('/users/{user}/toggleAdmin', [DashboardController::class, 'toggleAdmin'])->name('users.toggleAdmin')->middleware('admin');
-
-
+Route::get('/about', function () {return view('about');})->name('about');
 Route::get('/faq', [FaqController::class, 'index'])->name('faq.index');
-
-Route::post('/faq', [FaqController::class, 'store'])->name('faq.store')->middleware('admin');
-
-Route::post('/faq-answers', [FaqAnswerController::class, 'store'])->name('faqAnswer.store')->middleware('admin');
-
+Route::get('/categories/{name}', [CategoryController::class, 'show'])->name('categories.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile/{username}', [ProfileController::class, 'show'])->name('profile.show');
@@ -51,7 +33,15 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::post('/topics', [TopicController::class, 'store'])->name('topics.store');
+    Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
     Route::post('/reactions', [ReactionController::class, 'store'])->name('reactions.store');
+});
+
+Route::middleware('admin')->group(function () {
+    Route::post('/users/{user}/toggleAdmin', [DashboardController::class, 'toggleAdmin'])->name('users.toggleAdmin');
+    Route::get('/faq/create', [FaqController::class, 'create'])->name('faq.create');
+    Route::get('/faq-answers/create', [FaqAnswerController::class, 'create'])->name('faqAnswer.create');
 });
 
 require __DIR__.'/auth.php';
